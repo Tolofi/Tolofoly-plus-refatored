158a267df9752983aae7d8eb35051757143d74dd
unknown <Joo Gabriel>
2026-01-03 23:35:53 -0300

    pwa app try-3

--- Cliente/my-app/src/App.jsx
+++ Cliente/my-app/src/App.jsx
@@ -4,42 +4,31 @@ import { Login } from "./pages/Login";
 import { Wait } from "./pages/Wait";
 import "./Main.css";
 import { MainPage } from "./pages/MainPage";
-import { socket } from "../socket";
 import { useEffect } from "react";
-// import { Lobby } from "./screens/Lobby";
+// Importar o socket garante que ele inicialize e leia a URL do PWA
+import { socket } from "../socket";
 
-// Criei esse componente separado porque o useLocation só funciona DENTRO do BrowserRouter
 const RotasAnimadas = () => {
-  const location = useLocation(); // O segredo está aqui!
+  const location = useLocation();
 
   useEffect(() => {
-    // Escuta quando o app é aberto por uma URL (ex: tolofoly://server?ip=192...)
-    App.addListener("appUrlOpen", (data) => {
-      console.log("URL Recebida:", data.url);
+    // Como você está usando PWA, o "Inject" de IP já foi feito
+    // automaticamente no arquivo '../socket.js' através do window.location.search.
+    // Não precisamos mais do App.addListener aqui.
+    console.log("App PWA iniciado na rota:", location.pathname);
+  }, [location]);
 
-      // Lógica para extrair o IP ou comandos da URL
-      const url = new URL(data.url.replace("tolofoly:/", "https://fake.com"));
-      const serverIp = url.searchParams.get("ip");
-
-      if (serverIp) {
-        // Chame sua função de conectar ao socket aqui!
-        console.log("Conectando ao servidor:", serverIp);
-      }
-    });
-  }, []);
   return (
-    // mode="wait" significa: "Espere a velha sair para a nova entrar"
     <AnimatePresence mode="wait">
-      {/* O location e o key avisam o React que a página mudou */}
       <Routes location={location} key={location.pathname}>
         <Route path="/" element={<Login />} />
         <Route path="/wait" element={<Wait />} />
         <Route path="/main" element={<MainPage />} />
-        {/* <Route path="/lobby" element={<Lobby />} /> */}
       </Routes>
     </AnimatePresence>
   );
 };
+
 function App() {
   return (
     <BrowserRouter>
